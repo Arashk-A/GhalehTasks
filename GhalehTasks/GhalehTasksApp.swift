066dd30d9540f7e82e63d659da63c3d0a29674cd
zero <m.birang@gmail.com>
2024-10-31 17:29:24 +0330

    Initial Commit

---
+++ GhalehTasks/GhalehTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhalehTasksApp.swift
+//  GhalehTasks
+//
+//  Created by zero on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct GhalehTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
